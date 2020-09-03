9c7ebf89400dbc667da8040cb4d304bdac34f464
valery168 <70270704+valery168@users.noreply.github.com>
2020-09-03 16:27:05 +0300

    Add files via upload

---
+++ less3ex1.py
@@ -0,0 +1,10 @@
+def my_func():
+    s_1 = float(input('add nuber s_1 :'))
+    s_2 = float(input('add nuber s_2:'))
+    return s_1 / s_2
+
+print(my_func())
+
+
+
+
---
+++ less3ex4.py
@@ -0,0 +1,7 @@
+def my_func():
+   a = int(input('add number:'))
+   b = int(input("add negative number:"))
+   res = a ** b
+   return (res)
+
+print(my_func())
