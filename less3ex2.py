06b863ac7d661cb3cf31e5b3c3b7be53a345dd3a
valery168 <70270704+valery168@users.noreply.github.com>
2020-09-03 12:27:17 +0300

    Add files via upload

---
+++ less3ex2.py
@@ -0,0 +1,10 @@
+guest = {
+    'name': input('name'),
+    'soname': input('soname'),
+    'age': input('date of birth'),
+    'city': input('you city'),
+    'phone': input('phone number'),
+    'e-mail': input('you e-mail' )
+
+}
+print(guest)
